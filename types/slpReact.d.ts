42218c2df9a8df402f96be85f8db08e319165b2a
SaaS Update Script <saas.update.script@storelocatorplus.com>
2025-04-01 10:48:17 -0400

    Try making slpReact global.

--- types/slpReact.d.ts
+++ types/slpReact.d.ts
@@ -1,41 +1,42 @@
-/**
- * The slpReact JavaScript variable created in \SLP_Base_ReactObject::get_vars_for_react
- */
-interface mySLP {
-    api_key: string;
-    div_id: string;
-    embed_script: string;
-    location_count: bigint;
-    location_limit: bigint;
-    map_views: bigint;
-    map_views_limit: bigint;
-    map_views_overage: bigint;
-    map_views_over_limit: boolean;
-    map_views_over_message: string;
-    presentation: string;
-    rest_path: string;
-    site_url: string;
-    version: string;
-}
+export {}; // Ensures this is treated as a module.
 
-interface SLP {
-    apikey: string;
-    style_id: string;
-}
+declare global {
+    interface mySLP {
+        api_key: string;
+        div_id: string;
+        embed_script: string;
+        location_count: bigint;
+        location_limit: bigint;
+        map_views: bigint;
+        map_views_limit: bigint;
+        map_views_overage: bigint;
+        map_views_over_limit: boolean;
+        map_views_over_message: string;
+        presentation: string;
+        rest_path: string;
+        site_url: string;
+        version: string;
+    }
 
-interface url {
-    main_site: string;
-    rest: string;
-    slp_documentation: string;
-}
+    interface SLP {
+        apikey: string;
+        style_id: string;
+    }
 
-interface SLPReact {
-    nonce: string;
-    pageName: string;
-    SLP: SLP;
-    mySLP: mySLP;
-    scriptHandle: string;
-    url: url;
-}
+    interface url {
+        main_site: string;
+        rest: string;
+        slp_documentation: string;
+    }
 
-declare const slpReact: SLPReact;
+    interface SLPReact {
+        nonce: string;
+        pageName: string;
+        SLP: SLP;
+        mySLP: mySLP;
+        scriptHandle: string;
+        url: url;
+    }
+
+    const slpReact: SLPReact; // Declare global variable
+}
