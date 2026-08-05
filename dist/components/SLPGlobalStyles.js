b62597712f3eea01a18dd7e4994e889c4a473099
SaaS Update Script <saas.update.script@storelocatorplus.com>
2026-08-05 17:34:35 -0400

    Standardize React form field styles

--- dist/components/SLPGlobalStyles.js
+++ dist/components/SLPGlobalStyles.js
@@ -26,6 +26,60 @@ const SLPGlobalStyles = () => (_jsxs(_Fragment, { children: [_jsx(CssBaseline, {
                     outline: 'none !important',
                     boxShadow: 'none !important',
                 },
+                '.react-wrapper': {
+                    color: '#1d2327',
+                    fontFamily: 'Roboto, Arial, sans-serif',
+                    fontSize: '14px',
+                    lineHeight: 1.5,
+                },
+                '.react-wrapper .MuiInputLabel-root': {
+                    position: 'static',
+                    transform: 'none !important',
+                    maxWidth: 'none',
+                    marginBottom: '6px',
+                    color: 'rgba(0, 0, 0, 0.6)',
+                    fontSize: '0.875rem',
+                    fontWeight: 500,
+                    lineHeight: 1.4375,
+                    letterSpacing: '0.00938em',
+                    pointerEvents: 'auto',
+                },
+                '.react-wrapper .MuiInputLabel-root.Mui-focused': {
+                    color: '#1976d2',
+                },
+                '.react-wrapper .MuiInputLabel-root.Mui-error': {
+                    color: '#d32f2f',
+                },
+                '.react-wrapper .MuiOutlinedInput-root': {
+                    boxSizing: 'border-box',
+                    minHeight: '56px',
+                },
+                '.react-wrapper .MuiOutlinedInput-root:not(.MuiInputBase-multiline) .MuiOutlinedInput-input': {
+                    boxSizing: 'content-box',
+                    height: '1.4375em',
+                    minHeight: 0,
+                    padding: '16.5px 14px',
+                    font: 'inherit',
+                    lineHeight: '1.4375em',
+                },
+                '.react-wrapper .MuiOutlinedInput-root .MuiSelect-select.MuiOutlinedInput-input': {
+                    height: 'auto',
+                    minHeight: '1.4375em',
+                    padding: '16.5px 32px 16.5px 14px',
+                },
+                '.react-wrapper .MuiOutlinedInput-notchedOutline': {
+                    top: '0 !important',
+                },
+                '.react-wrapper .MuiOutlinedInput-notchedOutline legend': {
+                    maxWidth: '0 !important',
+                },
+                '.react-wrapper .MuiFormHelperText-root': {
+                    marginTop: '6px',
+                    marginRight: 0,
+                    marginLeft: 0,
+                    fontSize: '0.75rem',
+                    lineHeight: 1.5,
+                },
                 '.react-wrapper .MuiButton-root.MuiButton-contained:not(.Mui-disabled)': {
                     color: 'var(--variant-containedColor, #fff)',
                 },
